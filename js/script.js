d25d5876dbb71581dabd45b2a10edbc0e57934a7
Gillian Trethewey <cognitiveresults@rogers.com>
2022-05-19 18:08:21 -0400

    step 2

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const playAgainButton = document.querySelector(".play-again");
 const word = "magnolia";
 const guessedLetters = [];
 
-// Display our symbols as placeholders for the chosen word's letters
+// Make the not guessed letters circle placeholders
 const placeholder = function (word) {
     const placeholderLetters = [];
     for (const letter of word) {
@@ -24,15 +24,14 @@ placeholder(word);
 
 guessLetterButton.addEventListener("click", function (e) {
     e.preventDefault();
-    // Empty message paragraph
+    // reset message to empty
     message.innerText = "";
-    // Let's grab what was entered in the input
+    // guess contains the user input value
     const guess = letterInput.value;
-    // Let's make sure that it is a single letter
+    // validate the input
     const goodGuess = validateInput(guess);
 
     if (goodGuess) {
-        // We've got a letter! Let's guess!
         makeGuess(guess);
     }
     letterInput.value = "";
@@ -41,16 +40,12 @@ guessLetterButton.addEventListener("click", function (e) {
 const validateInput = function (input) {
     const acceptedLetter = /[a-zA-Z]/;
     if (input.length === 0) {
-        // Is the input empty?
         message.innerText = "Please enter a letter.";
     } else if (input.length > 1) {
-        // Did you type more than one letter?
         message.innerText = "Please enter a single letter.";
     } else if (!input.match(acceptedLetter)) {
-        // Did you type a number, a special character or some other non letter thing?
         message.innerText = "Please enter a letter from A to Z.";
     } else {
-        // We finally got a single letter, omg yay
         return input;
     }
 };
@@ -58,7 +53,7 @@ const validateInput = function (input) {
 const makeGuess = function (guess) {
     guess = guess.toUpperCase();
     if (guessedLetters.includes(guess)) {
-        message.innerText = "You already guessed that letter, silly. Try again.";
+        message.innerText = "Try again with a different letter.";
     } else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
